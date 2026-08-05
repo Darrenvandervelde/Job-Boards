75bab5a13940d37f7912c74da1ff29f038814d66
Darren van der Velde <darrenvandervelde@gmail.com>
2026-08-05 16:57:50 +0200

    updated test

--- api/jobs.js
+++ api/jobs.js
@@ -1,3 +1,5 @@
+/*
+
 import cors from "cors";
 import { scrapeLinkedIn } from "../scrapers/linkedin.js";
 
@@ -74,5 +76,17 @@ export default async function handler(req,res){
 
     });
 
+
+}
+    */
+
+export default function handler(req, res) {
+
+    console.log("API HIT");
+
+    res.status(200).json({
+        success: true,
+        message: "Vercel API working"
+    });
 
 }
